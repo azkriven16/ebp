6f7fde67518d58fe228702e304c94ff2290c564b
euger <azkriven17@gmail.com>
2023-06-12 19:56:38 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/Navbar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Animme",
+  description: "Watch Anime / Stream Anime Free",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ app/trending/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function TrendingPage() {
+  return <div>TrendingPage</div>;
+}
