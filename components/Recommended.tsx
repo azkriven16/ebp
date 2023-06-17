ace729ef5363d859c9593a4f16baa127d846a410
euger <azkriven17@gmail.com>
2023-06-17 19:32:17 +0800

    add null checks to recommended filter

--- components/Recommended.tsx
+++ components/Recommended.tsx
@@ -43,12 +43,12 @@ export default function Recommended() {
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
         {filter === "oldest"
           ? anime?.recommendations
-              .slice()
-              .reverse()
-              .map((ep: Anime) => {
+              ?.slice()
+              ?.reverse()
+              ?.map((ep: Anime) => {
                 return <AnimeCard anime={ep} />;
               })
-          : anime?.recommendations.map((ep: Anime) => {
+          : anime?.recommendations?.map((ep: Anime) => {
               return <AnimeCard anime={ep} />;
             })}
       </div>
