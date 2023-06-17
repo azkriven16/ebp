e78770358cc7bbc5b295affe49949eb839dd5ee5
euger <azkriven17@gmail.com>
2023-06-17 18:06:58 +0800

    set recent as main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
+"use client";
 import Hero from "@/components/Hero";
 import Recent from "@/components/Recent";
 import Trending from "@/components/Trending";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default async function HomePage() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/recent");
+  }, []);
   return (
     <main className="flex flex-col gap-10 mb-60">
       <Hero />
-      <Recent />
-      <Trending />
     </main>
   );
 }
