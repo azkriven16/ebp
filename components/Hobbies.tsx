53c73336e1ed330894d9213cbb0700dab73ae58e
euger <azkriven17@gmail.com>
2023-06-25 22:35:07 +0800

    removed fade from hobbies section

--- components/Hobbies.tsx
+++ components/Hobbies.tsx
@@ -1,18 +1,9 @@
-"use client";
 import Image from "next/image";
-import React, { useRef } from "react";
 import HobbiesAvatar from "@/public/hobbies.svg";
-import { useInView, motion } from "framer-motion";
-import { fadeVariant } from "./Animation";
 
 export default function Hobbies() {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
   return (
-    <motion.div
-      ref={ref}
-      className="flex flex-col gap-2 justify-center items-centerm mt-5"
-    >
+    <div className="flex flex-col gap-2 justify-center items-centerm mt-5">
       <div className="flex flex-col gap-2 justify-center items-start">
         <div className="flex items-center w-full gap-2 mb-5">
           <Image
@@ -26,6 +17,6 @@ export default function Hobbies() {
         </div>
         <p className="text-sm tracking-wider">Coding, Anime, Music, Manga</p>
       </div>
-    </motion.div>
+    </div>
   );
 }
