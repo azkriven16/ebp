635205398e3912ee1fc055f3623313b6cc3a40ff
euger <azkriven17@gmail.com>
2023-06-25 19:44:07 +0800

    added drei loader

--- components/Canvas.tsx
+++ components/Canvas.tsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Html, useProgress } from "@react-three/drei";
+import { OrbitControls, Loader } from "@react-three/drei";
 import Model from "./3d";
 
 export default function CanvasContainer() {
@@ -10,20 +10,14 @@ export default function CanvasContainer() {
       camera={{ position: [1, 7, 15], rotation: [0, 0, 0], fov: 45 }}
     >
       <ambientLight intensity={5} />
-      <Suspense fallback={<Loader />}>
-        <Model />
-      </Suspense>
+      <Model />
       <OrbitControls
         maxPolarAngle={Math.PI / 4} // Restrict rotation to 90 degrees up and down
         minPolarAngle={Math.PI / 4} // Restrict rotation to 90 degrees up and down
         minDistance={20} // Set the minimum zoom distance
         maxDistance={25} // Set the maximum zoom distance
       />
+      <Loader />
     </Canvas>
   );
-}
-
-function Loader() {
-  const { progress } = useProgress();
-  return <Html center>{progress} % loaded</Html>;
 }
